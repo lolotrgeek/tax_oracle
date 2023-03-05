18a3401647db42d8a9b9079ff70372afd330b4a2
lolotrgeek <jon@simplyenable.com>
2023-03-05 11:51:37 -0600

    feat: uses marginal rates for capital gains

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 const { married_joint } = require('./brackets.json')
 
 // a function that calculates income tax based on the brackets object above
-function calculateTax(income, brackets, bracketType, debug=false) {
-    let tax = 0;
-    let incomeLeft = income;
-    let bracketIndex = 0;
+function calculateTax(income, brackets, bracketType, debug = false) {
+    let tax = 0
+    let incomeLeft = income
+    let bracketIndex = 0
+    let income_rates = []
     while (incomeLeft > 0) {
         let bracket = brackets[bracketType][bracketIndex];
-        if(debug) console.log(bracket)
+        if (debug) console.log(bracket)
         if (!bracket.income_max) {
             tax += incomeLeft * bracket.rate;
             incomeLeft = 0;
+            income_rates.push({ income: incomeLeft, rate: bracket.rate })
             break;
         }
         else {
             let incomeInBracket = Math.min(incomeLeft, bracket.income_max - bracket.income_min);
             tax += incomeInBracket * bracket.rate;
             incomeLeft -= incomeInBracket;
-            if(debug) console.log(`rate: ${bracket.rate} ${incomeLeft} tax: ${tax}`)
+            if (debug) console.log(`rate: ${bracket.rate} ${incomeLeft} tax: ${tax}`)
+            income_rates.push({ income: incomeInBracket, rate: bracket.rate })
+            if(incomeLeft === 0) break;
             if (bracketIndex < brackets[bracketType].length - 1) bracketIndex++
         }
+    }
+    let marginalRate = brackets[bracketType][bracketIndex].rate
+    return { amount: tax, marginalRate, income_rates };
+}
+
+function getMarginalRate(income, filing_status) {
+    if (filing_status === "married_joint") {
+        let federal_brackets = married_joint.federal_brackets
+        let state_brackets = married_joint.state_brackets
+        let federal_marginal_rate = calculateTax(income, federal_brackets, "ordinary_income").marginalRate
+        let state_marginal_rate = calculateTax(income, state_brackets, "ordinary_income").marginalRate
+        return { federal: federal_marginal_rate, state: state_marginal_rate }
 
     }
-    return tax;
+    else return { error: "not implemented" }
+}
+
+function getIncomeBrackets(income, filing_status) {
+    if (filing_status === "married_joint") {
+        let federal_brackets = married_joint.federal_brackets
+        let state_brackets = married_joint.state_brackets
+        return ({
+            federal: calculateTax(income, federal_brackets, "ordinary_income").income_rates,
+            state: calculateTax(income, state_brackets, "ordinary_income").income_rates
+        })
+
+    }
+    else return { error: "not implemented" }
 }
 
 /**
@@ -32,26 +61,31 @@ function calculateTax(income, brackets, bracketType, debug=false) {
  * @param {*} capital_gains 
  * @param {*} filing_status married_joint, married_separate, single, head_of_household
  */
-function calculateTaxes(earned_income=0, dividend_income=0, short_capital_gains=0, long_capital_gains=0, filing_status="married_joint") {
+function calculateTaxes(earned_income = 0, dividend_income = 0, short_capital_gains = 0, long_capital_gains = 0, filing_status = "married_joint") {
 
     if (filing_status === "married_joint") {
         let federal_brackets = married_joint.federal_brackets
         let state_brackets = married_joint.state_brackets
 
-        let ordinary_income = earned_income + dividend_income + short_capital_gains
-        let state_income = ordinary_income + long_capital_gains
+        let ordinary_income = earned_income + dividend_income
 
-        let ordinary_federal_taxes = calculateTax(earned_income, federal_brackets, "ordinary_income")
-        let capital_gains_federal_taxes = calculateTax(long_capital_gains, federal_brackets, "long_term_capital_gains")
-        let total_state_taxes = calculateTax(state_income, state_brackets, "ordinary_income")
-        
-        let total_taxes = ordinary_federal_taxes + capital_gains_federal_taxes + total_state_taxes
+        let federal_income = calculateTax(ordinary_income, federal_brackets, "ordinary_income")
+        let federal_short_capital_gains = short_capital_gains * federal_income.marginalRate
+        let federal_long_capital_gains = calculateTax(long_capital_gains, federal_brackets, "long_term_capital_gains").amount
+        let total_federal_taxes = federal_income.amount + federal_short_capital_gains + federal_long_capital_gains
+
+        let state_income = calculateTax(ordinary_income, state_brackets, "ordinary_income")
+        let state_capital_gains = (short_capital_gains + long_capital_gains) * state_income.marginalRate
+        let total_state_taxes = state_income.amount + state_capital_gains
+
+
+        let total_taxes = total_federal_taxes + total_state_taxes
         return total_taxes.toFixed(2)
 
     }
     else {
-        return("not implemented")
+        return ("not implemented")
     }
 }
 
-module.exports = { calculateTaxes }
+module.exports = { calculateTaxes, getMarginalRate, getIncomeBrackets }
--- test.js
+++ test.js
@@ -1,4 +1,16 @@
-const { calculateTaxes } = require("./main");
+const { calculateTaxes, getMarginalRate, getIncomeBrackets } = require("./main")
 
-console.log(calculateTaxes(100000, 0, 0, 0, "married_joint") === "18417.81" ? "pass" : "fail"  )
-console.log(calculateTaxes(100000, 15000, 20000, 10000, "married_joint") === "21117.81" ? "pass" : "fail"  )
+console.log(calculateTaxes(85000, 0, 0, 0, "married_joint"))
+console.log(calculateTaxes(85000, 15000, 5000, 10000, "married_joint"))
+
+
+console.log("--------------------")
+let invest_only = { divdend_income: 22590.68, short: 30000, long: 30000 }
+let total_invest = invest_only.divdend_income + invest_only.short + invest_only.long
+console.log("**Invest Only Taxes**")
+console.log("total" , total_invest)
+console.log("tax", calculateTaxes(0, invest_only.divdend_income, invest_only.short, invest_only.long, "married_joint"))
+console.log("--------------------")
+console.log("Marginal Rates", getMarginalRate(100000, "married_joint"))
+console.log("--------------------")
+console.log("Income Brackets", getIncomeBrackets(100000, "married_joint"))
